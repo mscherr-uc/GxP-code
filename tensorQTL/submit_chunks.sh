c2b100760960de1fce9399bdb97f93b11557bce3
mscherr-uc <mscherr@uchicago.edu>
2025-09-16 19:26:19 -0500

    Update submit_chunks.sh

--- tensorQTL/submit_chunks.sh
+++ tensorQTL/submit_chunks.sh
@@ -5,4 +5,4 @@ chunks=$1
 for i in $(seq 1 $chunks); do
     sbatch -q express -N1-1 -n 1 --mem=10G -t 1000 --job-name=$i --wrap "R --vanilla --args $i $chunks < mashr_compute_posterior_chunks.R"
 sleep 0.5
-done;
+done
